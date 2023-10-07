ec49327f23e52482617654f121595b46c26fa7cb
Jerry-Chang975 <jerry975607@gmail.com>
2023-10-08 00:33:56 +0800

    feat: add admin routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const restController = require('../controllers/restaurants-controller')
+const admin = require('./modules/admin')
+router.use('/admin', admin)
 
 router.get('/restaurants', restController.getRestaurants)
 
