d03cf1fb10b58d63eaadb87342d460cfac5bfb0e
Jerry-Chang975 <jerry975607@gmail.com>
2023-11-01 23:28:28 +0800

    feat: add restaurant detail page

--- controllers/restaurants-controller.js
+++ controllers/restaurants-controller.js
@@ -1,6 +1,12 @@
 const { Restaurant, Category } = require('../models')
 const restaurantController = {
-  getRestaurants: (req, res) => {
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, { include: Category, nest: true, raw: true }).then(restaurant => {
+      if (!restaurant) throw new Error('Restaurant dose not exist!')
+      return res.render('restaurant', { restaurant })
+    }).catch(err => next(err))
+  },
+  getRestaurants: (req, res, next) => {
     return Restaurant.findAll({
       include: Category,
       nest: true,
@@ -8,7 +14,7 @@ const restaurantController = {
     }).then(restaurants => {
       const data = restaurants.map(r => ({ ...r, description: r.description.substring(0, 50) }))
       return res.render('restaurants', { restaurants: data })
-    })
+    }).catch(err => next(err))
   }
 }
 module.exports = restaurantController
