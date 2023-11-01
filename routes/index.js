28a1ed82e6571057e4773415e9173aac707a9948
Jerry-Chang975 <jerry975607@gmail.com>
2023-11-01 23:01:31 +0800

    feat: add user restaurants page

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,7 @@ router.post(
   userController.signIn
 )
 router.get('/logout', userController.logout)
+
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
